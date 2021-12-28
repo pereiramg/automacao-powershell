7b55b630c3e2959af5d479428db2a321fc7b790b
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2021-12-27 22:57:05 -0300

    Disable o Delete e Options no HTTP Verbs IIS

--- iis/DisableDeleteOptionsHTTPVerbs.ps1
+++ iis/DisableDeleteOptionsHTTPVerbs.ps1
@@ -6,34 +6,16 @@
 
 Write-Host -ForegroundColor Green "========================================= Disable o Delete e Options no HTTP Verbs IIS ========================================="
 
-#Importando o modulo para validação
-Import-Module "$PSScriptRoot\..\winrm\TesteWinRM.ps1" -Force
-
 # Solicitando o arquivo com os servidores
 $entradaServidores = Get-Content (Read-Host "Insira o caminho e o nome do txt com os servidores: Ex: c:\temp\servidores.txt ")
 
 #Credenciais de acesso aos servidores
 $acessoServidores = Get-Credential -Message "Insira as credencias de acesso aos servidores"
-$user = $acessoServidores.UserName.ToUpper().Split("\")[-1]
-
-#Execução do teste de conectividade com o POwershell
-Write-Host -ForegroundColor Green "`n========================================= Validacao da porta WinRM ========================================="
-testWinRM $entradaServidores $user
-
-$data = Get-Date -Format "ddmmyyyy"
-if (Test-Path "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt"){
-    $servidoresOK = Get-Content "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt"
-    Write-Host -ForegroundColor Green "`nOs servidores que fecharam com sucesso:"
-    $servidoresOK
-}else{
-    Write-Host -ForegroundColor Red "`nNenhum servidor fechou com sucesso a conectividade com o WinRM"
-    Pause
-    Break
-}
 
 Write-Host -ForegroundColor Green "`n========================================= Executando as alteracoes ========================================="
-Invoke-Command -ComputerName $servidoresOK -Credential $acessoServidores -ScriptBlock{
+Invoke-Command -ComputerName $entradaServidores -Credential $acessoServidores -ScriptBlock{
     Add-WebConfigurationProperty -PSPath "MACHINE/WEBROOT/APPHOST" -Filter "system.webServer/security/requestFiltering" -Value @{VERB="OPTIONS";allowed="False"} -Name Verbs -AtIndex 0
     Add-WebConfigurationProperty -PSPath "MACHINE/WEBROOT/APPHOST" -Filter "system.webServer/security/requestFiltering" -Value @{VERB="DELETE";allowed="False"} -Name Verbs -AtIndex 1
 }
+
 Pause
