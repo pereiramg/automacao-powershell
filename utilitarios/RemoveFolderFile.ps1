df78124b190653c56dc6110a7eed6a18f8bd157b
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2021-12-30 20:37:18 -0300

    RemoveFolderFile.ps1

--- utilitarios/RemoveFolderFile.ps1
+++ utilitarios/RemoveFolderFile.ps1
@@ -6,40 +6,20 @@
 
 Write-Host -ForegroundColor Green "========================================= Deletar Folder ou Files ========================================="
 
-#Importando o modulo para validação
-Import-Module "$PSScriptRoot\..\winrm\TesteWinRM.ps1" -Force
-
 # Solicitando o arquivo com os servidores
 $entradaServidores = Get-Content (Read-Host "Insira o caminho e o nome do txt com os servidores: Ex: c:\temp\servidores.txt ")
 
 #Credenciais de acesso aos servidores
 $acessoServidores = Get-Credential -Message "Insira as credencias de acesso aos servidores"
-$user = $acessoServidores.UserName.ToUpper().Split("\")[-1]
-
-#Execução do teste de conectividade com o POwershell
-Write-Host -ForegroundColor Green "`n========================================= Validacao da porta WinRM ========================================="
-testWinRM $entradaServidores $user
-
-$data = Get-Date -Format "ddmmyyyy"
-if (Test-Path "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt") {
-    $servidoresOK = Get-Content "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt"
-    Write-Host -ForegroundColor Green "`nOs servidores que fecharam com sucesso:"
-    $servidoresOK
-}
-else {
-    Write-Host -ForegroundColor Red "`nNenhum servidor fechou com sucesso a conectividade com o WinRM"
-    Pause
-    Break
-}
 
 #Captura dos usuarios
 Write-Host -ForegroundColor Green "`n========================================= Informacao do Folder/File ========================================="
 $nomeFolderFile = Read-Host "Informe o caminho do arquivo ou pasta para remoção. EX.: c:\temp\Teste"
 
 Write-Host -ForegroundColor Green "`n========================================= Executando as alteracoes ========================================="
-Invoke-Command -ComputerName $servidoresOK -Credential $acessoServidores -ScriptBlock {
+Invoke-Command -ComputerName $entradaServidores -Credential $acessoServidores -ScriptBlock {
     Remove-Item -LiteralPath $using:nomeFolderFile -Force -Recurse -ErrorAction Ignore
     Write-Host "Realizado a remoção com sucesso do $using:nomeFolderFile em $env:COMPUTERNAME"
-
 }
+
 Pause
