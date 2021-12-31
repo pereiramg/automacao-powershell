b782eaee582dd0002939adda8583252046d6a189
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2021-12-30 21:57:23 -0300

    Verificar os status dos updates nos servidores

--- sccm/StatusUpdates.ps1
+++ sccm/StatusUpdates.ps1
@@ -1,41 +1,26 @@
 <#
-    Objetivo: Verificar o status dos updates nos servidores
+    Objetivo: Verificar os status dos updates nos servidores
     Version: 1.0
     Origem: https://timmyit.com/2016/08/01/sccm-and-powershell-force-install-of-software-updates-thats-available-on-client-through-wmi/
     Modificado: Marcelo Galdino Pereira
 #>
 
-Write-Host -ForegroundColor Green "========================================= Verificar o status dos updates nos servidores ========================================="
-
-#Importando o modulo para validação
-Import-Module "$PSScriptRoot\..\winrm\TesteWinRM.ps1" -Force
+Write-Host -ForegroundColor Green "========================================= Verificar os status dos updates nos servidores ========================================="
 
 # Solicitando o arquivo com os servidores
 $entradaServidores = Get-Content (Read-Host "Insira o caminho e o nome do txt com os servidores: Ex: c:\temp\servidores.txt")
 
 #Credenciais de acesso aos servidores
 $acessoServidores = Get-Credential -Message "Insira as credencias de acesso aos servidores"
-$user = $acessoServidores.UserName.ToUpper().Split("\")[-1]
-
-#Execução do teste de conectividade com o POwershell
-Write-Host -ForegroundColor Green "`n========================================= Validacao da porta WinRM ========================================="
-testWinRM $entradaServidores $user
 
 $data = Get-Date -Format "ddmmyyyy"
-if (Test-Path "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt") {
-    $servidoresOK = Get-Content "$PSScriptRoot\..\logs\$($user)ServerAccept-$($data).txt"
-    Write-Host -ForegroundColor Green "`nOs servidores que fecharam com sucesso:"
-    $servidoresOK
-}
-else {
-    Write-Host -ForegroundColor Red "`nNenhum servidor fechou com sucesso a conectividade com o WinRM"
-    Pause
-    Break
+
+# Criando a pasta c:\temp
+if (!(Test-Path "c:\temp")) {
+    New-Item -Path "c:\" -Name "temp" -ItemType "directory"
 }
 
 Write-Host -ForegroundColor Green "`n========================================= Executando as alteracoes ========================================="
-
-
 #Definição de variaveis
 $appEvalState0 = "0"
 $appEvalState1 = "1"
@@ -69,50 +54,50 @@ $appEvalState13 = "13"
     https://docs.microsoft.com/en-us/mem/configmgr/develop/reference/core/clients/sdk/ccm_softwareupdate-client-wmi-class
 #>
 
-foreach ($server in $servidoresOK) {
+foreach ($server in $entradaServidores) {
 
     $application = Get-WmiObject -Namespace "root\ccm\clientSDK" -Class CCM_SoftwareUpdate -ComputerName $server -Credential $acessoServidores
 
     if (!$application) {
-        Write-Output "$server - Nenhum Update Pendente" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Nenhum Update Pendente" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStateNone
     if ($application.EvaluationState -eq $appEvalState0 ) {
-        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStateAvailable
     if ($application.EvaluationState -eq $appEvalState1 ) {
-        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
     
     #ciJobStateDownloading
     if ($application.EvaluationState -eq $appEvalState5 ) {
-        Write-Output "$server - Processo de download de updates" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Processo de download de updates" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStateInstall
     if ($application.EvaluationState -eq $appEvalState7 ) {
-        Write-Output "$server - Sem Updates Pendentes" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Sem Updates Pendentes" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStatePendingSoftReboot
     if ($application.EvaluationState -eq $appEvalState8 ) {
-        Write-Output "$server - Pendente boot" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Pendente boot" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStatePendingHardReboot
     if ($application.EvaluationState -eq $appEvalState9 ) {
-        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Updates Pendentes" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 
     #ciJobStateError
     if ($application.EvaluationState -eq $appEvalState13 ) {
-        Write-Output "$server - Erro na instalacao dos updates" | Tee-Object -Append "$PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+        Write-Output "$server - Erro na instalacao dos updates" | Tee-Object -Append "c:\temp\sccm-Status_Updates$($data).txt"
     }
 }
 
-Write-Host -ForegroundColor Green "`n`n O arquivo foi gerado no caminho $PSScriptRoot\..\logs\$($user)_Status_Updates$($data).txt"
+Write-Host -ForegroundColor Green "`n`n O arquivo foi gerado no caminho c:\temp\sccm-Status_Updates$($data).txt"
 
 Pause
