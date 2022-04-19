4113dc76b8521902eae4c131450d7010ee748907
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-04-19 14:28:46 -0300

    discos-rdm-report.ps1

--- vmware/discos-rdm-report.ps1
+++ vmware/discos-rdm-report.ps1
@@ -4,15 +4,38 @@
     Autor: Marcelo Galdino Pereira
 #>
 
+#import de module
+Write-Host "`nImportando o modulo do PowerCli"
+Import-Module VMware.VimAutomation.Core -ErrorAction SilentlyContinue
+
+Write-Host "`n`n =================================== Captura de informações =================================== " -ForegroundColor Green
+
+$servidores = Get-Content(Read-Host "Insira o caminho do arquivo txt com os dados dos servidores")
+
+$senhaVMware = Get-Credential -Message "Insira usuario e senha para acesso ao VMware"
+$vcenter = Read-Host "Insira o nome do VCenter para se conectar"
+
+Write-Host "`n`n=================== Conectando no $vcenter  =======================`n`n"
+    do{
+        try{
+            Connect-VIServer $vcenter -Credential $senhaVMware -Force
+        }catch [System.ServiceModel.Security.SecurityNegotiationException]{
+            Write-Host "Realizando nova tentativa de conexão" -ForegroundColor Yellow
+        }
+
+        try{
+            $lasterror = [string]$Error[0].Exception.GetType().FullName
+        }catch [System.Management.Automation.RuntimeException]{
+            $lasterror = " "
+        }$Error.Clear()
+    }until( $lasterror -ne "System.ServiceModel.Security.SecurityNegotiationException")
+
+    Write-Host "Conectado com sucesso no $vcenter"
+
+
 Write-Host "`n`n=================== Extrair relatorios de discos RDM ==================`n`n"
 
-
-Connect-VIServer 
-
 $report = @()
-
-# Arquivo txt com a lista dos servidores
-$servidores = Get-Content C:\temp\servidores.txt
 
 foreach($vm in $servidores){
     $vm = Get-VM $vm | Get-View
